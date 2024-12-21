19e233e87abecc17a440ba3ce45cadb59e4eb367
Nicolas-Saade <nicolasnsaade@gmail.com>
2024-12-20 22:58:51 -0800

    Refactored VITE env Vars

--- GameShop/GameShop-FrontEnd/src/utils/apiFetch.js
+++ GameShop/GameShop-FrontEnd/src/utils/apiFetch.js
@@ -1,7 +1,7 @@
-const baseURL = process.env.REACT_APP_BASE_URL || 'http://localhost:8080';
+const baseURL = import.meta.env.VITE_BASE_URL || 'http://localhost:8080';
 
 const apiFetch = async (endpoint, options = {}) => {
-  console.log("REEEEEAAAccctttt", process.env.REACT_APP_BASE_URL);
+  console.log("REEEEEAAAccctttt", import.meta.env.VITE_BASE_URL);
   const response = await fetch(`${baseURL}${endpoint}`, options);
 
   if (!response.ok) {
